1d794edd10556978401b8060507fdba80b6b132d
sinsect <sin@odin.proj>
2022-06-27 11:30:23 -0400

    remove space between squares, add hover effect

--- script.js
+++ script.js
@@ -2,16 +2,21 @@ const gridContainer = document.querySelector("#grid-container")
 
 let rowCount = 16;
 let colCount = 16; 
+gridContainer.style.lineHeight = '0px';
 for (let i = 0; i < rowCount; i++) {
     for (let j = 0; j < colCount; j++) {
         let div = document.createElement('div');
+        div.classList.add('gridDiv');
         let divSquareSize = '20px'
         div.style.minWidth = divSquareSize;
         div.style.minHeight = divSquareSize;
         div.style.display = 'inline-block';
-        div.style.border = 'solid';
         div.style.backgroundColor = 'pink';
-        div.style.marginRight = '4px';
+        div.style.border = 'solid';
+        div.style.borderWidth = '1px';
+        div.addEventListener(('mouseover'), () => {
+            div.style.backgroundColor = 'red';
+        });
         gridContainer.appendChild(div);
     }
     let breakLine = document.createElement('br');
