1b70bd09fd73148a2c967ec5ce91a6e6b6dfb3dc
sinsect <sin@odin.proj>
2022-06-27 14:59:11 -0400

    add resize function, move some styling to css

--- script.js
+++ script.js
@@ -1,24 +1,42 @@
-const gridContainer = document.querySelector("#grid-container")
-
+const gridContainer = document.querySelector('#grid-container');
 let rowCount = 16;
-let colCount = 16; 
-gridContainer.style.lineHeight = '0px';
-for (let i = 0; i < rowCount; i++) {
-    for (let j = 0; j < colCount; j++) {
-        let div = document.createElement('div');
-        div.classList.add('gridDiv');
-        let divSquareSize = '20px'
-        div.style.minWidth = divSquareSize;
-        div.style.minHeight = divSquareSize;
-        div.style.display = 'inline-block';
-        div.style.backgroundColor = 'pink';
-        div.style.border = 'solid';
-        div.style.borderWidth = '1px';
-        div.addEventListener(('mouseover'), () => {
-            div.style.backgroundColor = 'red';
-        });
-        gridContainer.appendChild(div);
+let colCount = 16;
+let containerWidth = 500;
+gridContainer.style.width = containerWidth + 'px';
+gridContainer.style.height = gridContainer.style.width;
+//create grid of pink squares, size default 16x16
+function makeGrid() {
+    for (let i = 0; i < rowCount; i++) {
+        for (let j = 0; j < colCount; j++) {
+            let div = document.createElement('div');
+            div.classList.add('gridDiv');
+            //turn squares red on hover
+            div.addEventListener(('mouseover'), () => {
+                div.style.backgroundColor = 'red';
+            });
+            gridContainer.appendChild(div);
+        }
+        let breakLine = document.createElement('br');
+        gridContainer.appendChild(breakLine);
     }
-    let breakLine = document.createElement('br');
-    gridContainer.appendChild(breakLine);
 }
+function deleteGrid() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+makeGrid();
+const resizeButton = document.querySelector('#resize-button');
+resizeButton.addEventListener(('click'), () => {
+    let currentSize = colCount;
+    let newSize = prompt('dimensions? (max 100)', currentSize);
+    if (!isNaN(newSize) && newSize >= 0 && newSize <= 100) {
+        colCount = newSize;
+        rowCount = newSize;
+        deleteGrid();
+        makeGrid();
+    }
+    else {
+        prompt('Invalid input!');
+    }
+});
